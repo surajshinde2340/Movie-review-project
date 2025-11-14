5128b58efbf6a4ab861f5f60d71acc665a42969e
surajshinde2340 <surajshinde2340@gmail.com>
2025-11-14 09:39:55 +0530

    merege

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const server = express();
+const router = require("./routes/login");
 
+server.use( express.json() );
+server.use( router );
 
 server.listen( 6000 , ()=>{ 
     console.log(" server run fine ");
